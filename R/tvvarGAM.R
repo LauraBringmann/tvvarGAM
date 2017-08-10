c7e127ea2b51f3457b13d1cee8f9b3249d3a045f
jmbh <jonashaslbeck@gmail.com>
2017-08-10 17:37:40 +0200

    some cleaning up and performance improvements

--- R/tvvarGAM.R
+++ R/tvvarGAM.R
@@ -1,40 +1,59 @@
 
 
 
-tvvarGAM <- function(data,
-                     nv,
-                     nt,
-                     nb=10,
+tvvarGAM <- function(data, # the n x p data matrix
+                     nb = 10,
+                     consec,
                      SIMdata,
-                     simulated=TRUE,
-                     plot=TRUE,
-                     estimates=FALSE){ #data is the data,nt is the number of time points, nv=number of variables and k is the number of knots
+                     simulated = FALSE,
+                     plot = FALSE,
+                     estimates = TRUE,
+                     pbar){ #data is the data,nt is the number of time points, nv=number of variables and k is the number of knots
 
-  if (estimates==TRUE)
-  {
+
+
+  # --------- Fill in defaults ---------
+
+  if(missing(pbar)) pbar <- TRUE
+  if(missing(consec)) consec <- 1:nrow(data) # if not provided, assume all measurements are subsequent
+
+  # --------- Compute Aux Variables ---------
+
+  nt <- nrow(data)
+  nv <- ncol(data)
+  tt=1:nt
+
+
+  # --------- Case I: estimates = TRUE ---------
+
+  if(estimates==TRUE) {
+
+
     Results_GAM<-array(NA,c(c(nv+1,nv),nt,3))
 
-
-    #nt=100 # 100 500
-    tt=1:nt
 
     #%##########################################%###
     ####  Part 1: creating the data #############
     #%##########################################%##
     #The functions in this part are created in the file (source code for simulation article 3.R)
 
-
-
-    y<-data
-
+    y <- data
 
     #%##########################################%###
     ####  Part 2: ESTIMATING GAM##### #############
     #%##########################################%##
 
+    mod_all <- tvvarGAM(data = y,
+                        nb = nb,
+                        SIMdata = FALSE,
+                        simulated = FALSE,
+                        plot = FALSE,
+                        estimates = FALSE,
+                        pbar = TRUE)
+
     for (ii in 1:nv){
 
-      mod<-tvvarGAM(y,nv,nt,nb,SIMdata = FALSE,plot=FALSE)[[ii]]
+      mod <- mod_all[[ii]]
 
       mat_dat<-matrix(c(tt,rep(rep(1,nt),nv)),length(tt),nv+1)
       coln_Data<-paste("y",1:nv,"L",sep="")
@@ -76,86 +95,121 @@ tvvarGAM <- function(data,
 
     }
     return(Results_GAM=outlist)
-  }
-
-  else {
 
 
 
+    # --------- Case I: estimates = FALSE ---------
 
-  tt=1:nt
-
-  Data1=matrix(0,nt,(nv*2),byrow=T)
-  for (h in (nv+1):(nv*2)){
-    Data1[,(h-nv)]=data[,(h-nv)]# data wordt hier gelagged
-
-    Data1[,h]=c(NA,data[1:(nt-1),(h-nv)])# data wordt hier gelagged
-  }
-
-  Data1=as.data.frame(Data1)
-  colnames(Data1)=c(paste("y",1:nv,sep=""),paste("y",1:nv,"L",sep=""))
-
-  coln=colnames(Data1)[1:nv]
-  colnL=colnames(Data1)[(nv+1):(nv*2)]
-  allcol2=c()
-  for(i in 1:nv){allcol2[i]=paste("s(tt,by=",colnL[i],",k=nb",")",sep="")}
-  allcol3=paste(allcol2,collapse="+")
-
-
-  model=list()
-  for (j in 1:nv){
-    ff <- as.formula(paste(coln[j]," ~ ","s(tt,k=nb)","+",allcol3))
-    model[[j]]<-gam(ff,data=Data1,seWithMean=TRUE)
-
-  }
+  } else {
 
 
 
+    # Lag data by Laura
 
-  if(plot==TRUE & simulated==TRUE){
-  par(mfrow=c(nv,(nv+1)))
-  tt=1:nt
-  for (i in 1:nv){
-    mod<-model[[i]]
-    k=0
+    Data1=matrix(0,nt,(nv*2),byrow=T)
+    for (h in (nv+1):(nv*2)){
+      Data1[,(h-nv)]=data[,(h-nv)]# data wordt hier gelagged
 
-    for ( j in 1:(nv+1))
-      if(j==1)
-      {plot.gam(mod,seWithMean = TRUE,select=1,rug=F,shift=coef(mod)[1],ylab="intercept")
-        lines(tt,SIMdata$aint[,i],col="red")
-      }
-    else {plot.gam(mod,seWithMean = TRUE,select=j,rug=F,ylim=c(-1,1))
-      k=1+k
+      Data1[,h]=c(NA,data[1:(nt-1),(h-nv)])# data wordt hier gelagged
+    }
 
-      lines(tt,SIMdata$rho[,k],col="red")
+    Data1=as.data.frame(Data1)
+    colnames(Data1)=c(paste("y",1:nv,sep=""),paste("y",1:nv,"L",sep=""))
+
+
+    # Instead use lagData() from the mgm package
+
+    lagD_obj <- mgm:::lagData(data,
+                              lags = 1,
+                              consec = consec)
+
+
+    # Back to Laura's variable names:
+    Data2 <- cbind(lagD_obj$data_response, lagD_obj$l_data_lags[[1]])
+    Data2 <- rbind(rep(NA, ncol(Data2)), Data2) # to make Laura's code below work
+    Data2 <- as.data.frame(Data2)
+    colnames(Data2)=c(paste("y",1:nv,sep=""),paste("y",1:nv,"L",sep=""))
+    Data1 <- Data2
+
+    coln=colnames(Data1)[1:nv]
+    colnL=colnames(Data1)[(nv+1):(nv*2)]
+    allcol2=c()
+
+
+    for(i in 1:nv){allcol2[i]=paste("s(tt,by=",colnL[i],",k=nb",")",sep="")}
+    allcol3=paste(allcol2,collapse="+")
+
+
+    # Progress bar
+    if(pbar==TRUE) pb <- txtProgressBar(min = 0, max=nv, initial=0, char="-", style = 3)
+
+    model=list()
+    for (j in 1:nv){
+
+      ff <- as.formula(paste(coln[j]," ~ ","s(tt,k=nb)","+",allcol3))
+      model[[j]]<-gam(ff,data=Data1,seWithMean=TRUE)
+
+      # Update Progress Bar
+      if(pbar==TRUE) setTxtProgressBar(pb, j)
+
 
     }
-  }
-  }
-
-  else if (plot==TRUE) {  par(mfrow=c(nv,(nv+1)))
-    for (i in 1:nv){
-      mod<-model[[i]]
-      k=0
-
-      for ( j in 1:(nv+1))
-        if(j==1)
-        {plot.gam(mod,seWithMean = TRUE,select=1,rug=F,shift=coef(mod)[1],ylab="intercept")
-               }
-      else {plot.gam(mod,seWithMean = TRUE,select=j,rug=F,ylim=c(-1,1))
-        k=1+k
 
 
+
+
+
+
+
+
+
+    # --------- Plotting Stuff ---------
+
+
+    if(plot==TRUE & simulated==TRUE){
+      par(mfrow=c(nv,(nv+1)))
+      tt=1:nt
+      for (i in 1:nv){
+        mod<-model[[i]]
+        k=0
+
+        for ( j in 1:(nv+1))
+          if(j==1)
+          {plot.gam(mod,seWithMean = TRUE,select=1,rug=F,shift=coef(mod)[1],ylab="intercept")
+            lines(tt,SIMdata$aint[,i],col="red")
+          }
+        else {plot.gam(mod,seWithMean = TRUE,select=j,rug=F,ylim=c(-1,1))
+          k=1+k
+
+          lines(tt,SIMdata$rho[,k],col="red")
+
+        }
       }
+    }  else if (plot==TRUE) {  par(mfrow=c(nv,(nv+1)))
+      for (i in 1:nv){
+        mod<-model[[i]]
+        k=0
+
+        for ( j in 1:(nv+1))
+          if(j==1)
+          {plot.gam(mod,seWithMean = TRUE,select=1,rug=F,shift=coef(mod)[1],ylab="intercept")
+          }
+        else {plot.gam(mod,seWithMean = TRUE,select=j,rug=F,ylim=c(-1,1))
+          k=1+k
+
+
+        }
+      }
+    } else {
+
+
+      # Return Estimates
+
+      return(model)
+
     }
-  }
-
-  else {
-    return(model)
 
   }
-
-}
 }
 
 
